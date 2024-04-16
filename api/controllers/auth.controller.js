c15b1c78b0cf6b30d7683543ba9322541e3cae76
Utkarsh <utkarshmishra101394@gmail.com>
2024-04-17 02:23:57 +0530

    create sign-in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 const User = require('../models/user.model')
 // bcryptjs is the npm package used for hashing the passwords
 const bcryptjs = require("bcryptjs");
+const errorHandler = require('../utils/error');
+const jwt = require("jsonwebtoken");
 
 
 const signup = async (req, res, next) => {
@@ -15,4 +17,22 @@ const signup = async (req, res, next) => {
     }
   };
 
-module.exports = signup;
+const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+      const validUser = await User.findOne({ email });
+      if (!validUser) return next(errorHandler(404, 'User not found!'));
+      const validPassword = bcryptjs.compareSync(password, validUser.password);
+      if (!validPassword) return next(errorHandler(401, 'Invalid Password'));
+      const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+      const { password: pass, ...rest } = validUser._doc;
+      res
+        .cookie('access_token', token, { httpOnly: true })
+        .status(200)
+        .json(rest);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+module.exports = {signup, signin};
