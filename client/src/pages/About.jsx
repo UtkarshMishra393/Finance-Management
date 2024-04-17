0c5102c9265089d7ac60adfe206adffbb87ecc67
Utkarsh <utkarshmishra101394@gmail.com>
2024-04-17 11:45:50 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-yellow-500'>About Finance Manager</h1>
+      <p className='mb-4 text-yellow-600 font-semibold'> <span className='text-lg font-bold mb-4 text-yellow-800'>Our Mission:</span> At Finance Manager, we are dedicated to empowering individuals to take control of their financial well-being. We believe that sound financial management is the cornerstone of a fulfilling life, and our mission is to provide the tools, resources, and guidance necessary for everyone to achieve their financial goals.</p>
+      <p className='mb-4 text-yellow-600 font-semibold'>
+        <span className='text-lg font-bold mb-4 text-yellow-800'>What We Offer:</span> Through our platform, users gain access to comprehensive financial planning tools, budgeting assistance, investment guidance, and personalized recommendations tailored to their unique financial situation. Whether you're looking to build wealth, save for a specific goal, or simply gain a better understanding of your finances, we're here to support you every step of the way.
+      </p>
+      <p className='mb-4 text-yellow-600 font-semibold'> <span className='text-lg font-bold mb-4 text-yellow-800'>Why Choose Us:</span> With a team of experienced financial experts and a commitment to transparency, security, and user satisfaction, Finance Manager stands out as a trusted partner in personal finance management. We prioritize your financial success and strive to make managing your money as seamless, informative, and rewarding as possible. Join us on the journey to financial empowerment today!</p>
+    </div>
   )
 }
